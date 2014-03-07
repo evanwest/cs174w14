7a1249707ceac120721d0680e67a4015b8875246
Evan West <e_west@umail.ucsb.edu>
2014-03-06 21:09:05 -0800

    added some directory structure, jdbc JAR, server connect test

---
+++ cs174w14/src/cs174w14/model/JdbcTest.java
@@ -0,0 +1,33 @@
+package cs174w14.model;
+
+import java.sql.*;
+
+public class JdbcTest {
+
+	public static String serverAddress="jdbc:oracle:thin:@uml.cs.ucsb.edu:1521:xe";
+	public static void main(String[] args) {
+		ResultSet rs;
+		Connection conn;
+		try{
+			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+			conn = DriverManager.getConnection(serverAddress, "e_west", "4871406");
+
+			Statement st = conn.createStatement();
+			rs = st.executeQuery("SELECT owner, table_name FROM all_tables");
+			
+			while(rs.next()){
+				System.out.println(rs.getString("owner")+","+rs.getString("table_name"));
+			}
+			
+			rs.close();
+			conn.close();
+		} catch (SQLException sqle){
+			sqle.printStackTrace();
+		}
+	}
+	
+	public static void setUpTables(){
+		
+	}
+
+}
---
+++ cs174w14/src/cs174w14/model/Tables.java
@@ -0,0 +1,12 @@
+package cs174w14.model;
+
+public class Tables {
+	
+		public static final String[] tableNames = {
+			"mart_Products" , "Descriptions", "Accessories", "Orders",
+			"Order_Items", "Customers", "Cart_Items", "Loyalty_Statuses",
+				
+			"depot_Products", 
+		};
+
+}
